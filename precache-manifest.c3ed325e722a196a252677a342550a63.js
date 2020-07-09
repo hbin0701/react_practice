f6f93ff27d662890c8a392afad6155cf4f474a1f
Ronal Hwang <hbin0701@kaist.ac.kr>
2020-07-09 17:29:44 +0900

    My first commit! not really :)

---
+++ precache-manifest.c3ed325e722a196a252677a342550a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee5d1833dbe27eb3f21a900899bd72c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c797c68e048e7cf5c78",
+    "url": "/static/css/main.18fd41b6.chunk.css"
+  },
+  {
+    "revision": "1142420c3f528ac8c384",
+    "url": "/static/js/2.1ca6d722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ca6d722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c797c68e048e7cf5c78",
+    "url": "/static/js/main.4a0f0264.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
